bf1f84c279b5767e92323168f2147c72b1424800
Holden Karau <holden@pigscanfly.ca>
2020-05-02 17:37:42 -0700

    Install some things so we can set up falco more easily

--- make_k8s_cluster/update_pi.sh
+++ make_k8s_cluster/update_pi.sh
@@ -3,6 +3,18 @@ DEBIAN_FRONTEND=noninteractive
 export DEBIAN_FRONTEND
 apt-get update
 apt-get upgrade -y
-# Set up falco
-apt-get install -y tmate
+# This makes debugging less work
+apt-get install -y tmate emacs nano
+# Necessary build magics
+# Note: QEMU means we're running a different kernel
+# than the one we use on the board
+apt install -y linux-headers-raspi2  cmake gcc clang libyaml-cpp-dev libyaml-dev pkgconfig
+# Install falco
+git clone https://github.com/falcosecurity/falco.git
+pushd falco
+git pull
+# This is from nova
+git checkout origin/falco-on-arm
+# Setup K3s
+
 

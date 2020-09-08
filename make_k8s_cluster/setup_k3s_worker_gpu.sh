7c589d0e1a465d97f0ab91a0f99ab303c329206f
Holden Karau <holden@pigscanfly.ca>
2020-09-07 20:19:45 -0700

    Don't prompt cleaning the docker network and specify versions for the CSI container images

--- make_k8s_cluster/setup_k3s_worker_gpu.sh
+++ make_k8s_cluster/setup_k3s_worker_gpu.sh
@@ -2,7 +2,7 @@
 set -ex
 # Clean up docker
 docker system prune -af || echo "k"
-docker network prune || echo "k"
+docker network prune -f || echo "k"
 INSTALL_K3S_EXEC="--docker"
 export INSTALL_K3S_EXEC
 K3S_EXTRA="--node-label role=gpu"

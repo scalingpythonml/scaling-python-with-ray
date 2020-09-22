f6b2215f6d56d93ef56d3c87ff4a1f2660cdf610
Holden Karau <holden@pigscanfly.ca>
2020-09-21 19:55:27 -0700

    Update rook/ceph, take out jetson baking, etc.

--- make_k8s_cluster/bootstrap-local.sh
+++ make_k8s_cluster/bootstrap-local.sh
@@ -43,7 +43,7 @@ if [ ! -f images/ubuntu-arm64-leader.img ]; then
   ${COPY_COMMAND} -af rook ubuntu-image/
   ${COPY_COMMAND} setup_*.sh ubuntu-image/
   # Note operator.yaml change v2.1.1 to v2.1.1-arm64
-  ${COPY_COMMAND} rook_*.yaml ubuntu-image/rook/rook/cluster/examples/kubernetes/ceph/
+  ${COPY_COMMAND} rook_*.yaml ubuntu-image/rook/cluster/examples/kubernetes/ceph/
   # The leader has a worker counter file
   ${COPY_COMMAND} worker_counter.txt ubuntu-image/
   cleanup_misc
@@ -62,36 +62,4 @@ if [ ! -f images/ubuntu-arm64-leader.img ]; then
   cleanup_ubuntu_mounts
   sudo kpartx -dv images/ubuntu-arm64-worker.img
   sync
-fi
-echo "Baking jetson nano worker image"
-unset firmware_boot_partition
-if [ -z "${jpid}" ]; then
-  wait ${jpid} || echo "dl done"
-fi
-if [ ! -f images/sd-blob-b01.img ]; then
-  pushd images; unzip jetson-nano.zip; popd
-fi
-if [ ! -f images/jetson-nano-custom.img ]; then
-  cp images/sd-blob-b01.img images/jetson-nano-custom.img
-  setup_jetson_img images/jetson-nano-custom.img
-  setup_ubuntu_mounts
-  cleanup_ubuntu_mounts
-  resize_partition images/jetson-nano-custom.img 1 ${JETSON_TARGET_SIZE}
-  setup_jetson_img images/jetson-nano-custom.img
-  setup_ubuntu_mounts
-  enable_chroot
-  update_ubuntu
-  config_system
-  if [ -f jetson_docker_daemon.json.custom ]; then
-    ${COPY_COMMAND} jetson_docker_daemon.json.custom ubuntu-image/etc/docker/daemon.json
-  else
-    ${COPY_COMMAND} jetson_docker_daemon.json ubuntu-image/etc/docker/daemon.json
-  fi
-  ${COPY_COMMAND} firstboot.sh ubuntu-image/etc/init.d/firstboot
-  ${COPY_COMMAND} first_run_worker.sh ubuntu-image/do_firstboot.sh
-  ${COPY_COMMAND} setup_k3s_worker_gpu.sh ubuntu-image/setup_k3s_worker.sh
-  ${RUN_DEST_CMD} update-rc.d  firstboot defaults
-  copy_ssh_keys
-  cleanup_ubuntu_mounts
-  sudo kpartx -dv images/jetson-nano-custom.img
 fi
--- make_k8s_cluster/setup_rook.sh
+++ make_k8s_cluster/setup_rook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pushd /rook/rook/cluster/examples/kubernetes/ceph
+pushd /rook/cluster/examples/kubernetes/ceph
 kubectl create -f common.yaml
 kubectl create -f rook_operator_arm64.yaml
 kubectl create -f rook_cluster.yaml

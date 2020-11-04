68d626381816d6f8dbd3c6701e9f21fc55d6602b
Holden Karau <holden@pigscanfly.ca>
2020-11-03 19:00:23 -0800

    Tag the namespace setup and tag and update the conda setup

--- dask-examples/setup.sh
+++ dask-examples/setup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 # Get our versions to match as much as possible easily.
-sudo /opt/conda/bin/conda install -c conda-forge --all --yes     python==3.8     cytoolz     dask==2.28.0     lz4     nomkl     numpy     pandas     tini     scikit-build     python-blosc
-
+#tag::setup_conda[]
+sudo /opt/conda/bin/conda install -c conda-forge --all --yes mamba
+mamba install --yes python==3.8.6 cytoolz dask==2.30.0 dask-core==2.30.0 lz4 numpy==1.19.2 pandas tini \
+      scikit-build python-blosc=1.9.2 pyzmq s3fs gcsfs dropboxdrivefs requests dropbox paramiko adlfs \
+      pygit2 pyarrow bokeh aiottp==3.7.1 llvmlite numba fastparquet
+#end::setup_conda[]
 
 # Create a namespace & SA & permissions.
-#kubectl create namespace dask
-#kubectl create serviceaccount dask --namespace dask
-#kubectl apply -f setup.yaml
-#kubectl create rolebinding dask-sa-binding --namespace dask --role=daskKubernetes --user=dask:dask
+#tag::setup_namespace[]
+kubectl create namespace dask
+kubectl create serviceaccount dask --namespace dask
+kubectl apply -f setup.yaml
+kubectl create rolebinding dask-sa-binding --namespace dask --role=daskKubernetes --user=dask:dask
+#end::setup_namespace[]
 
 # Set up SSL trust of the k3s hosts?
 scp root@k8s-leader.local:/var/lib/rancher/k3s/agent/client-ca.crt ~/k3s-ca.crt

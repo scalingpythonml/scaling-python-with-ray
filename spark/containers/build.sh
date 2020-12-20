768d0f5c1cd5cfae3678734b23575cb1bb7fd2a0
Holden Karau <holden@pigscanfly.ca>
2020-12-20 13:52:49 -0800

    Update the tags and explain -j 4

--- spark/containers/build.sh
+++ spark/containers/build.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 set -eux
+#tag::dlspark[]
 if [ ! -f spark-3.0.1-bin-hadoop3.2.tgz ]; then
   axel https://ftp.wayne.edu/apache/spark/spark-3.0.1/spark-3.0.1-bin-hadoop3.2.tgz
 fi
 if [ ! -d spark-3.0.1-bin-hadoop3.2 ]; then
   tar -xvf spark-3.0.1-bin-hadoop3.2.tgz
 fi
+#end::dlspark[]
 cp ./notebook/* ./spark-3.0.1-bin-hadoop3.2
 cp ./python-executor/Dockerfile ./spark-3.0.1-bin-hadoop3.2/PyDockerfile
 # Fixed to do buildx push
 cp ./docker-image-*.sh ./spark-3.0.1-bin-hadoop3.2/bin/
+#tag::build_exec_containers[]
 # Copy over python setup script so we can have matching pythons
 cp pysetup.sh ./spark-3.0.1-bin-hadoop3.2/bin/
 pushd spark-3.0.1-bin-hadoop3.2
 SPARK_HOME=`pwd`
 SPARK_ROOT="$SPARK_HOME"
-docker buildx build -t holdenk/spark-notebook:v3.0.1.2  --platform linux/arm64,linux/amd64 --push .
 ./bin/docker-image-tool.sh  -r holdenk -t v3.0.1.2 -X -b java_image_tag=11-jre-slim -p PyDockerfile Dockerfile build
+#end::build_exec_containers[]
+#tag::build-notebook[]
+docker buildx build -t holdenk/spark-notebook:v3.0.1.2  --platform linux/arm64,linux/amd64 --push .
+#end::build-notebook[]
 popd

7a9cc1f2ac334921601ca679c63d698277bc052a
Holden Karau <holden@pigscanfly.ca>
2021-06-15 21:58:16 -0700

    Update the container build to add S3a & iceberg & use 3.1.1 from Spark

--- spark/containers/build.sh
+++ spark/containers/build.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 set -eux
 #tag::dlspark[]
-if [ ! -f spark-3.0.1-bin-hadoop3.2.tgz ]; then
-  axel https://ftp.wayne.edu/apache/spark/spark-3.0.1/spark-3.0.1-bin-hadoop3.2.tgz
+if [ ! -f spark-3.1.1-bin-hadoop3.2.tgz ]; then
+  axel https://ftp.wayne.edu/apache/spark/spark-3.1.1/spark-3.1.1-bin-hadoop3.2.tgz
 fi
-if [ ! -d spark-3.0.1-bin-hadoop3.2 ]; then
-  tar -xvf spark-3.0.1-bin-hadoop3.2.tgz
+rm -rf spark-3.1.1-bin-hadoop-3.2
+if [ ! -d spark-3.1.1-bin-hadoop3.2 ]; then
+  tar -xvf spark-3.1.1-bin-hadoop3.2.tgz
 fi
 # Set SPARK_HOME to extracted directory
-#SPARK_HOME=`pwd`/spark-3.0.1-bin-hadoop3.2
+SPARK_HOME=`pwd`/spark-3.1.1-bin-hadoop3.2
 #end::dlspark[]
 # Temporary
-if [ ! -d spark-3.1.2-SNAPSHOT-bin-3.2.0 ]; then
-  tar -xvf spark-3.1.2-SNAPSHOT-bin-3.2.0.tgz
-fi
-SPARK_HOME=`pwd`/spark-3.1.2-SNAPSHOT-bin-3.2.0
+#if [ ! -d spark-3.1.2-SNAPSHOT-bin-3.2.0 ]; then
+#  tar -xvf spark-3.1.2-SNAPSHOT-bin-3.2.0.tgz
+#fi
+#SPARK_HOME=`pwd`/spark-3.1.2-SNAPSHOT-bin-3.2.0
 cp ./notebook/* ${SPARK_HOME}
 cp ./python-executor/Dockerfile ${SPARK_HOME}/PyDockerfile
 #tag::build_exec_containers[]
 # Copy over python setup script so we can have matching pythons
-SPARK_VERSION=3.1.1.1-preview
+SPARK_VERSION=3.1.1.11
 cp pysetup.sh ${SPARK_HOME}/bin/
 pushd ${SPARK_HOME}
 SPARK_ROOT="$SPARK_HOME"

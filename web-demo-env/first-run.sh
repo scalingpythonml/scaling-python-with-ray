507d102d3d489b10198ebbb3cd3bd77fd3244584
Holden Karau <holden@pigscanfly.ca>
2021-09-28 18:45:19 -0700

    List the notebooks too just incase start-book is being smart.

--- web-demo-env/first-run.sh
+++ web-demo-env/first-run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # On codespaces using jupyter as the entrypoint doesn't work so great
 # 
+set -x
 if [ ! -f /tmp/running ] || [ $(kill -s 0 $(cat /tmp/running)) ]; then
   start-notebook.sh --NotebookApp.allow_origin='*' &
   echo $! > /tmp/running
+  jupyter notebook list
 fi

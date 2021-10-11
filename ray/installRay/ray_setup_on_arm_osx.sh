61eb0dfe81513228886dea48a9f09114405b80a7
Holden Karau <holden@pigscanfly.ca>
2021-10-11 12:51:43 -0700

    Tag the osx deps install

--- ray/installRay/ray_setup_on_arm_osx.sh
+++ ray/installRay/ray_setup_on_arm_osx.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#tag::deps[]
 brew install bazelisk wget python@3.8 npm
 # Make sure homebrew Python is used before system Python
 export PATH=$(brew --prefix)/opt/python@3.8/bin/:$PATH
@@ -6,3 +7,4 @@ echo "export PATH=$(brew --prefix)/opt/python@3.8/bin/:$PATH" >> ~/.zshrc
 echo "export PATH=$(brew --prefix)/opt/python@3.8/bin/:$PATH" >> ~/.bashrc
 # Install some libraries vendored incorrectly by Ray for ARM
 pip3 install --user psutil cython colorama
+#end::deps[]

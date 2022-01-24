1027c73ff4f7664908ef3cd3f0967710ede31169
blublinsky <blublinsky@hotmail.com>
2022-01-23 18:25:50 -0600

    Add ML code

---
+++ ray_examples/ml/boosting/wine-quality-xgboost.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import time
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+
+import xgboost as xgb
+
+# Get data
+df = pd.read_csv("winequality-red.csv", delimiter=";")
+print(f"Rows, columns: {str(df.shape)}")
+print(df.head)
+print(df.isna().sum())
+
+# Create Classification version of target variable
+df['goodquality'] = [1 if x >= 6 else 0 for x in df['quality']]
+X = df.drop(['quality','goodquality'], axis = 1)
+y = df['goodquality']
+print(df['goodquality'].value_counts())
+
+# Normalize feature variables
+X_features = X
+X = StandardScaler().fit_transform(X)
+# Splitting the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.25, random_state=0)
+
+start = time.time()
+model = xgb.XGBClassifier(random_state=1)
+model.fit(X_train, y_train)
+print(f"executed XGBoost in {time.time() - start}")
+y_pred = model.predict(X_test)
+print(classification_report(y_test, y_pred))

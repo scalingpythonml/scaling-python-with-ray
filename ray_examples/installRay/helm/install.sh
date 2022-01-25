85c66f4e8f4b07cccc8fd5a757f45aa97953f30d
Holden Karau <holden@pigscanfly.ca>
2022-01-24 18:01:22 -0800

    Fix our Ray helm install.

--- ray_examples/installRay/helm/install.sh
+++ ray_examples/installRay/helm/install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+set -ex
 export SCALING_PYTHON_ML_EXAMPLES_PATH=$(realpath "$(dirname "$0")")
+if [ ! -d ray ]; then
 #tag::add_ray_chart[]
 git clone git@github.com:ray-project/ray.git
 pushd ray/deploy/charts
 #end::add_ray_chart[]
+else
+pushd ray/deploy/charts
+fi
 # Deploy operator
 #tag::deploy_operator[]
 helm install ray-operator  --set operatorOnly=true ./ray
 #end::deploy_operator[]
 #tag::deploy_cluster[]
-helm install gpu-cluster -n ray --create-namespace --set clusterOnly=true -f ${SCALING_PYTHON_ML_EXAMPLES_PATH}/ray/installRay/helm/helm_config_selector.yaml ./ray
+helm install gpu-cluster -n ray --create-namespace --set clusterOnly=true -f ${SCALING_PYTHON_ML_EXAMPLES_PATH}/helm_config_selector.yaml ./ray
 #end::deploy_cluster[]

88ba53aa9685708e38443224768d5e9e6fc7ce99
blublinsky <blublinsky@hotmail.com>
2022-01-27 10:52:42 -0600

    Add ML/Torch code

---
+++ ray_examples/ml/torch/wine-quality_ltorch_ray.py
@@ -0,0 +1,135 @@
+import pandas as pd
+from numpy import vstack
+import time
+from sklearn.metrics import accuracy_score
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+from torch.nn import Linear
+from torch.nn import ReLU
+from torch.nn import Sigmoid
+from torch.optim import Adam
+from torch.nn import BCELoss
+from torch.nn.init import kaiming_uniform_
+from torch.nn.init import xavier_uniform_
+from pytorch_lightning.core.lightning import LightningModule
+from pytorch_lightning import Trainer, seed_everything
+
+from ray_lightning import RayPlugin
+
+# Dataset
+class CSVDataset(Dataset):
+    # load the dataset
+    def __init__(self, path):
+        # load the csv file as a dataframe
+        df = pd.read_csv(path, delimiter=";")
+        print(f"Rows, columns: {str(df.shape)}")
+        print(df.head)
+        # Create Classification version of target variable
+        df['goodquality'] = [1 if x >= 6 else 0 for x in df['quality']]
+        df = df.drop(['quality'], axis = 1)
+        print(df['goodquality'].value_counts())
+        # store the inputs and outputs
+        self.X = df.values[:, :-1]
+        self.y = df.values[:, -1]
+        # ensure input data is floats
+        self.X = self.X.astype('float32')
+        self.y = self.y.astype('float32')
+        self.y = self.y.reshape((len(self.y), 1))
+
+    # number of rows in the dataset
+    def __len__(self):
+        return len(self.X)
+
+    # get a row at an index
+    def __getitem__(self, idx):
+        return [self.X[idx], self.y[idx]]
+
+    # get indexes for train and test rows
+    def get_splits(self, n_test=0.33):
+        # determine sizes
+        test_size = round(n_test * len(self.X))
+        train_size = len(self.X) - test_size
+        # calculate the split
+        return random_split(self, [train_size, test_size])
+
+# model definition
+class WineQuality(LightningModule):
+    # define model elements
+    def __init__(self, n_inputs):
+        super(WineQuality, self).__init__()
+        # input to first hidden layer
+        self.hidden1 = Linear(n_inputs, 10)
+        kaiming_uniform_(self.hidden1.weight, nonlinearity='relu')
+        self.act1 = ReLU()
+        # second hidden layer
+        self.hidden2 = Linear(10, 8)
+        kaiming_uniform_(self.hidden2.weight, nonlinearity='relu')
+        self.act2 = ReLU()
+        # third hidden layer and output
+        self.hidden3 = Linear(8, 1)
+        xavier_uniform_(self.hidden3.weight)
+        self.act3 = Sigmoid()
+        self.bce = BCELoss()
+
+    # forward propagate input
+    def forward(self, X):
+        # input to first hidden layer
+        X = self.hidden1(X)
+        X = self.act1(X)
+        # second hidden layer
+        X = self.hidden2(X)
+        X = self.act2(X)
+        # third hidden layer and output
+        X = self.hidden3(X)
+        X = self.act3(X)
+        return X
+    # training step
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.bce(y_hat, y)
+        return loss
+    # optimizer
+    def configure_optimizers(self):
+        return Adam(self.parameters(), lr=0.02)
+# Ensure reproducibility
+seed_everything(42)
+
+# load the dataset
+dataset = CSVDataset("winequality-red.csv")
+
+# calculate split
+train, test = dataset.get_splits()
+# prepare data loaders
+train_dl = DataLoader(train, batch_size=32, shuffle=True)
+test_dl = DataLoader(test, batch_size=32, shuffle=False)
+
+# Train the model
+model = WineQuality(11)
+# define the optimization
+start = time.time()
+# train
+plugin = RayPlugin(num_workers=6)
+trainer = Trainer(max_steps=1000, plugins=[plugin])
+trainer.fit(model, train_dl)
+print(f"Build model in {time.time() - start}")
+print(model)
+# evaluate a model
+predictions, actuals = list(), list()
+for i, (inputs, targets) in enumerate(test_dl):
+    # evaluate the model on the test set
+    yhat = model(inputs)
+    # retrieve numpy array
+    yhat = yhat.detach().numpy()
+    actual = targets.numpy()
+    actual = actual.reshape((len(actual), 1))
+    # round to class values
+    yhat = yhat.round()
+    # store
+    predictions.append(yhat)
+    actuals.append(actual)
+predictions, actuals = vstack(predictions), vstack(actuals)
+# calculate accuracy
+acc = accuracy_score(actuals, predictions)
+print("Model accuracy", acc)

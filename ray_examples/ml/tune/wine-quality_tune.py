946de0a95c24d478eae519c54aa12a623eb2d9e4
blublinsky <blublinsky@hotmail.com>
2022-02-20 17:29:34 -0600

    Add Tune code

---
+++ ray_examples/ml/tune/wine-quality_tune.py
@@ -0,0 +1,166 @@
+import pandas as pd
+import numpy as np
+from numpy import vstack
+
+import torch
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+from torch.nn import Linear
+from torch.nn import ReLU
+from torch.nn import Sigmoid
+from torch.nn import Module
+from torch.optim import SGD
+from torch.nn import BCELoss
+from torch.nn.init import kaiming_uniform_
+from torch.nn.init import xavier_uniform_
+
+from ray import tune
+from ray.tune.schedulers import ASHAScheduler
+
+# Dataset
+class WineQualityDataset(Dataset):
+    # load the dataset
+    def __init__(self, path):
+        # load the csv file as a dataframe
+        df = pd.read_csv(path, delimiter=";")
+        print(f"Rows, columns: {str(df.shape)}")
+        print(df.head)
+        # Create Classification version of target variable
+        df['goodquality'] = [1 if x >= 6 else 0 for x in df['quality']]
+        df = df.drop(['quality'], axis = 1)
+        print(df['goodquality'].value_counts())
+        # store the inputs and outputs
+        self.X = StandardScaler().fit_transform(df.values[:, :-1])
+        self.y = df.values[:, -1]
+        # ensure input data is floats
+        self.X = self.X.astype('float32')
+        self.y = self.y.astype('float32')
+        self.y = self.y.reshape((len(self.y), 1))
+
+    # number of rows in the dataset
+    def __len__(self):
+        return len(self.X)
+
+    # get a row at an index
+    def __getitem__(self, idx):
+        return [self.X[idx], self.y[idx]]
+
+    # get indexes for train and test rows
+    def get_splits(self, n_test=0.33):
+        # determine sizes
+        test_size = round(n_test * len(self.X))
+        train_size = len(self.X) - test_size
+        # calculate the split
+        return random_split(self, [train_size, test_size])
+
+# model definition
+class WineQualityModel(Module):
+    # define model elements
+    def __init__(self, n_inputs):
+        super(WineQualityModel, self).__init__()
+        # input to first hidden layer
+        self.hidden1 = Linear(n_inputs, 10)
+        kaiming_uniform_(self.hidden1.weight, nonlinearity='relu')
+        self.act1 = ReLU()
+        # second hidden layer
+        self.hidden2 = Linear(10, 8)
+        kaiming_uniform_(self.hidden2.weight, nonlinearity='relu')
+        self.act2 = ReLU()
+        # third hidden layer and output
+        self.hidden3 = Linear(8, 1)
+        xavier_uniform_(self.hidden3.weight)
+        self.act3 = Sigmoid()
+
+    # forward propagate input
+    def forward(self, X):
+        # input to first hidden layer
+        X = self.hidden1(X)
+        X = self.act1(X)
+        # second hidden layer
+        X = self.hidden2(X)
+        X = self.act2(X)
+        # third hidden layer and output
+        X = self.hidden3(X)
+        X = self.act3(X)
+        return X
+
+# train function
+def model_train(model, optimizer, criterion, train_loader):
+    # for every mini batches
+    for i, (inputs, targets) in enumerate(train_loader):
+        # clear the gradients
+        optimizer.zero_grad()
+        # compute the model output
+        yhat = model(inputs)
+        # calculate loss
+        loss = criterion(yhat, targets)
+        # credit assignment
+        loss.backward()
+        # update model weights
+        optimizer.step()
+
+# test model
+def model_test(model, test_loader):
+    predictions, actuals = list(), list()
+    for i, (inputs, targets) in enumerate(test_loader):
+        # evaluate the model on the test set
+        yhat = model(inputs)
+        # retrieve numpy array
+        yhat = yhat.detach().numpy()
+        actual = targets.numpy()
+        actual = actual.reshape((len(actual), 1))
+        # round to class values
+        yhat = yhat.round()
+        # store
+        predictions.append(yhat)
+        actuals.append(actual)
+    predictions, actuals = vstack(predictions), vstack(actuals)
+    # calculate accuracy
+    return accuracy_score(actuals, predictions)
+
+# Train wine quality model
+def train_winequality(config):
+
+    # calculate split
+    train, test = dataset.get_splits()
+    train_dl = DataLoader(train, batch_size=32, shuffle=True)
+    test_dl = DataLoader(test, batch_size=32, shuffle=False)
+
+    # Model
+    model = WineQualityModel(11)
+    # define the optimization
+    criterion = BCELoss()
+    optimizer = SGD(
+        model.parameters(), lr=config["lr"], momentum=config["momentum"])
+    for i in range(50):
+        model_train(model, optimizer, criterion, train_dl)
+        acc = model_test(model, test_dl)
+
+        # Send the current training result back to Tune
+        tune.report(mean_accuracy=acc)
+
+        if i % 5 == 0:
+            # This saves the model to the trial directory
+            torch.save(model.state_dict(), "./model.pth")
+
+# ensure reprodusability
+torch.manual_seed(42)
+# load the dataset
+dataset = WineQualityDataset("winequality-red.csv")
+
+search_space = {
+    "lr": tune.sample_from(lambda spec: 10**(-10 * np.random.rand())),
+    "momentum": tune.uniform(0.1, 0.9)
+}
+
+analysis = tune.run(
+    train_winequality,
+    num_samples=50,
+    scheduler=ASHAScheduler(metric="mean_accuracy", mode="max"),
+    config=search_space
+)
+
+print(f"Done {analysis}")

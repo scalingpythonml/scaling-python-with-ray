57a9f794399fbf8c3b814891eab1c7bd8cf7c132
Holden Karau <holden@pigscanfly.ca>
2022-04-24 23:05:02 -0700

    Try and make cuda on l4t work.

--- spark/containers/build.sh
+++ spark/containers/build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -eux
 #tag::dlspark[]
-if [ ! -f spark-3.1.2-bin-hadoop3.2.tgz ]; then
-  axel https://ftp.wayne.edu/apache/spark/spark-3.1.2/spark-3.1.2-bin-hadoop3.2.tgz
+if [ ! -f spark-3.2.1-bin-hadoop3.2.tgz ]; then
+  axel https://ftp.wayne.edu/apache/spark/spark-3.2.1/spark-3.2.1-bin-hadoop3.2.tgz
 fi
-rm -rf spark-3.1.2-bin-hadoop-3.2
-if [ ! -d spark-3.1.2-bin-hadoop3.2 ]; then
-  tar -xvf spark-3.1.2-bin-hadoop3.2.tgz
+rm -rf spark-3.2.1-bin-hadoop-3.2
+if [ ! -d spark-3.2.1-bin-hadoop3.2 ]; then
+  tar -xvf spark-3.2.1-bin-hadoop3.2.tgz
 fi
 # Set SPARK_HOME to extracted directory
-SPARK_HOME=`pwd`/spark-3.1.2-bin-hadoop3.2
+SPARK_HOME=`pwd`/spark-3.2.1-bin-hadoop3.2
 #end::dlspark[]
 # Temporary
 #if [ ! -d spark-3.1.2-SNAPSHOT-bin-3.2.0 ]; then
@@ -20,7 +20,7 @@ cp ./notebook/* ${SPARK_HOME}
 cp ./python-executor/Dockerfile ${SPARK_HOME}/PyDockerfile
 #tag::build_exec_containers[]
 # Copy over python setup script so we can have matching pythons
-SPARK_VERSION=3.1.2.1
+SPARK_VERSION=3.2.1.1
 cp pysetup.sh ${SPARK_HOME}/bin/
 pushd ${SPARK_HOME}
 SPARK_ROOT="$SPARK_HOME"

e39b90ae998c639ccc628f25e7333393c699e14c
Ivan Siroshtan <ivan.siroshtan@onlystudio.org>
2022-05-12 11:53:14 +0300

    feat(blocked numbers): Add blocked numbers model and view #27934

---
+++ src/apps/core/views/blocked_numbers.py
@@ -0,0 +1,58 @@
+from django import forms
+from django.core.paginator import Paginator
+from django.shortcuts import redirect, render
+from django.urls import reverse, reverse_lazy
+from django.views import View
+from django.views.generic import DeleteView
+
+from apps.core.models import BlockedNumber
+
+
+class BlockedNumberForm(forms.Form):
+    number = forms.CharField(required=True)
+
+
+class BlockedNumbersView(View):
+    template = "blocked_numbers.html"
+    form_class = BlockedNumberForm
+    page_size = 10
+
+    def get(self, request, extra_context=None):
+        if extra_context is None:
+            extra_context = {}
+        numbers = BlockedNumber.object.get_user_blocked_numbers(
+            request.user
+        ).values("id", "number")
+        paginator = Paginator(numbers, self.page_size)
+        page = paginator.get_page(request.GET.get("page", 1))
+        form = self.form_class()
+        return render(
+            request,
+            self.template,
+            {**self.base_context, "page": page, "form": form, **extra_context},
+        )
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            BlockedNumber.object.add_user_blocked_number(
+                form.cleaned_data["number"], request.user
+            )
+            return self.get(request)
+        return self.get(request, {"form": form, "error": True})
+
+    @property
+    def base_context(self):
+        return {
+            "title": "Blocked numbers",
+            "navname": "Blocked numbers",
+        }
+
+
+class DeleteBlockedNumberView(View):
+    def post(self, request, pk):
+        BlockedNumber.object.delete_user_blocked_number(pk, request.user)
+        return redirect(reverse("core:blocked-numbers"))
+
+
+__all__ = ["DeleteBlockedNumberView", "BlockedNumbersView"]

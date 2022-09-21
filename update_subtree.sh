8defcdee93ebf927f0250ecc3f783db94877abb9
Holden Karau <holden@pigscanfly.ca>
2022-09-21 04:04:56 -0700

    Fix msg backend ray.

--- update_subtree.sh
+++ update_subtree.sh
@@ -5,6 +5,6 @@ if [ ! -d examples ]; then
 fi
 git subtree pull  --prefix=examples git@github.com:scalingpythonml/scalingpythonml.git master
 if [ ! -d message-backend-ray ]; then
-  git subtree add  --prefix=message-backend-ray git@github.com:pigscanflylabs/message-backend-ray.git master
+  git subtree add  --prefix=message-backend-ray git@github.com:PigsCanFlyLabs/message-backend-ray.git main
 fi
-git subtree pull  --prefix=message-backend-ray git@github.com:pigscanflylabs/message-backend-ray.git master
+git subtree pull  --prefix=message-backend-ray git@github.com:PigsCanFlyLabs/message-backend-ray.git main

c9d3167091ed3aa542b617d233117d6a22a63e0d
Holden Karau <holden@pigscanfly.ca>
2022-09-21 04:03:56 -0700

    Add message backend.

--- update_subtree.sh
+++ update_subtree.sh
@@ -4,3 +4,7 @@ if [ ! -d examples ]; then
   git subtree add  --prefix=examples git@github.com:scalingpythonml/scalingpythonml.git master
 fi
 git subtree pull  --prefix=examples git@github.com:scalingpythonml/scalingpythonml.git master
+if [ ! -d message-backend-ray ]; then
+  git subtree add  --prefix=message-backend-ray git@github.com:pigscanflylabs/message-backend-ray.git master
+fi
+git subtree pull  --prefix=message-backend-ray git@github.com:pigscanflylabs/message-backend-ray.git master

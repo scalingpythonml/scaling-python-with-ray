b31991f6ec32466b8e64cd9b33dd2e06059829fa
Holden Karau <holden@pigscanfly.ca>
2022-09-26 05:21:48 -0700

    Fix handling of file names with spaces in convert notebooks.

--- convert_notebooks.sh
+++ convert_notebooks.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -ex
+# Needed for files with spaces.
+OIFS="$IFS"
+IFS=$'\n'
 for file in $(find . -name "*ipynb" |grep -v venv |grep -v .ipynb_checkpoints); do
   jupyter nbconvert --to script $file || echo "Bad code?"
 done
+IFS="$OIFS"
